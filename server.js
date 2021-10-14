14895f093f79472a42fefa9c29f57ebb70576a30
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-10-13 21:49:22 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -56,14 +56,6 @@ app.get("/api/animals", (req, res) => {
     res.json(results);
 });
 
-app.get("/api/animals/:id", (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
 
 app.listen(3001, () => {
     console.log("API server now on port 3001!");
